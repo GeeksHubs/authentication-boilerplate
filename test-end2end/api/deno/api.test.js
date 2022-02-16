b45ca18e3efd414ad009a7aeb115d260fabff675
Jupegarnica <juan@geekshubs.com>
2022-02-16 11:46:42 +0100

    fix(test): deno tests

--- test-end2end/api/deno/api.test.js
+++ test-end2end/api/deno/api.test.js
@@ -1,27 +1,27 @@
 import { assertEquals, config } from "./deps.js";
 
 const { API_HOST } = config({ path: "./.env" });
+
 console.log({ API_HOST });
 
 
-// Deno.test('mock', async () => {
-//     const response = await fetch('https://mock-api.deno.dev/?body=hola');
-//     const body = await response.text();
-//     assertEquals(body, 'hola');
-//     assertEquals(response.status, 200);
+Deno.test('mock', async () => {
+    const response = await fetch('https://mock-api.deno.dev/?body=hola');
+    const body = await response.text();
+    assertEquals(body, 'hola');
+    assertEquals(response.status, 200);
 
-//     //
-// })
-
+})
 
 
 
-// Deno.test({
-//     name: 'localhost',
-//     ignore: false,
-//     fn: async () => {
-//         const response = await fetch(API_HOST + '/users');
-//         await response.body.cancel();
-//         assertEquals(response.status, 200);
-//     }
-// })
+
+Deno.test({
+    name: 'localhost',
+    ignore: false,
+    fn: async () => {
+        const response = await fetch(API_HOST + '/users');
+        await response.body.cancel();
+        assertEquals(response.status, 200);
+    }
+})
