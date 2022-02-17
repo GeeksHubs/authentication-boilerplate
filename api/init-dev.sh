0952cbcc9b96a72dfbd7d7f1f6ce7dce991c6c9a
Jupegarnica <juan@geekshubs.com>
2022-02-17 11:13:48 +0100

    chore(api): clean up

--- api/init-dev.sh
+++ api/init-dev.sh
@@ -3,30 +3,6 @@ echo "--------------------------"
 echo "Runnnig init.sh"
 echo "--------------------------"
 
-# Rename .env init DockerCompose
-#echo "Rename Variable init"
-#cp .env.example .env
-
-#Up docker Compose.
-#echo "Docker-compose up"
-#docker-compose up
-#if [ ! $? -eq 0 ]; then
-#    echo "Couldn't start service or Control-C was pressed"
-#    echo "cleaning up"
-#    docker-compose down
-#    exit $?
-#fi
-
-#####################################################################
-###                   DATABASES                                  ####
-#####################################################################
-# echo "--------------------------"
-# echo "CREATE DATABASES"
-# echo "--------------------------"
-
-# docker exec -ti pcex_bd sh -c "ulimit -n 900000"
-# docker exec -ti pcex_bd sh -c "cd /opt/bd && mysql -uroot  -p145687545ERF!  < 01-databases.sql"
-
 #####################################################################
 ###                   API                                        ####
 #####################################################################
@@ -60,6 +36,3 @@ docker exec -ti authentication_api  sh -c "cd bootstrap && chown -R www-data:www
 docker exec -ti authentication_api  sh -c "cd bootstrap && chmod -R 777 cache"
 #Generate api Doc
 # docker exec -ti authentication_api sh -c "php artisan l5-swagger:generate"
-#Ejecute create JWT Key
-#echo "Add jwt Key"
-#docker exec -ti api-gateway_php7 sh -c "php artisan jwt:secret"
